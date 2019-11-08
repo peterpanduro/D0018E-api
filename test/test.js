684c715481443166ce907ced190b88ca67a8bad5
Peter <peter@kodifiera.se>
2019-11-08 20:57:59 +0100

    Added tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Tests', function() {
+    describe('Basic tests', function() {
+      it('Should return true', function() {
+        assert.ok(true);
+      });
+    });
+  });
+  
