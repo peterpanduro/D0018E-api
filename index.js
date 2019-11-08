34c8ab7399a1fa5d476775b8750d49d701b8530d
Peter <peter@kodifiera.se>
2019-11-08 22:56:27 +0100

    Added database connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
+
+const dbConnection = mysql.createConnection({
+    host: '37.123.183.130',
+    port: '3306',
+    user: 'stefan',
+    password: 'backend',
+    database: 'd0018e_test'
+})
 
 // API endpoints
 app.get('/', (req, res) => {
@@ -8,4 +17,13 @@ app.get('/', (req, res) => {
 })
 
 // Run server
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    dbConnection.connect(function(err) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        console.log('Database connected');
+    })
+})
