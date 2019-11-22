d2c379b8a5efd291794387a94921e1dc7723ad90
Peter Panduro <peter@kodifiera.se>
2019-11-22 11:54:43 +0100

    Testing always-up documentation

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const mysql = require('mysql')
 const app = express()
 const port = 3000
+const swaggerUi = require('swagger-ui-express')
+const fs = require('fs')
+const jsyaml = require('js-yaml')
+const spec = fs.readFileSync('swagger.yaml', 'utf8')
+const swaggerDoc = jsyaml.safeLoad(spec)
 
 const dbConnection = mysql.createConnection({
     host: '37.123.183.130',
@@ -15,6 +20,7 @@ const dbConnection = mysql.createConnection({
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Run server
 app.listen(port, () => {
